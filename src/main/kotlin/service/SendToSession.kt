c28163fea84976ed6686200d834a2f0ff994441b
antllcon <pokemonivan32@gmail.com>
2025-07-14 13:15:37 +0300

    Небольшие правки

--- src/main/kotlin/service/SendToSession.kt
+++ src/main/kotlin/service/SendToSession.kt
@@ -3,15 +3,41 @@ package mobility.service
 import io.ktor.websocket.WebSocketSession
 import io.ktor.websocket.send
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
 import mobility.model.ErrorResponse
 import mobility.model.ServerException
 import mobility.model.ServerMessage
+import mobility.model.*
 
 val AppJson = Json {
     encodeDefaults = true
     ignoreUnknownKeys = true
     prettyPrint = false
     classDiscriminator = "kind"
+
+    // Регистрация интерфейсов и их подклассов для полиморфной сериализации/десериализации
+    serializersModule = SerializersModule {
+        polymorphic(ClientMessage::class) {
+            subclass(InitPlayerRequest::class)
+            subclass(CreateRoomRequest::class)
+            subclass(JoinRoomRequest::class)
+            subclass(LeaveRoomRequest::class)
+            subclass(PlayerActionRequest::class)
+        }
+        polymorphic(ServerMessage::class) {
+            subclass(PlayerConnectedResponse::class)
+            subclass(PlayerDisconnectedResponse::class)
+            subclass(InfoResponse::class)
+            subclass(ErrorResponse::class)
+            subclass(RoomCreatedResponse::class)
+            subclass(JoinedRoomResponse::class)
+            subclass(LeftRoomResponse::class)
+            subclass(RoomUpdatedResponse::class)
+            subclass(PlayerActionResponse::class)
+        }
+    }
 }
 
 suspend fun sendToSession(session: WebSocketSession, message: ServerMessage) {
