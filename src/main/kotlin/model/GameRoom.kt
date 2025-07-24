cb10c6b041450cb9c8bb3fc15098ca29c88a2137
Gleb Ryzhov <glebka.r@yandex.ru>
2025-07-25 02:08:15 +0300

    fixed countdown

--- src/main/kotlin/model/GameRoom.kt
+++ src/main/kotlin/model/GameRoom.kt
@@ -52,7 +52,7 @@ class GameRoom(
     fun startCountdown(handler: GameWebSocketHandler) {
         scope.launch {
             while (secondsBeforeStart > 0) {
-                secondsBeforeStart -= serverTickRate / 1000L
+                secondsBeforeStart -= serverTickRate / 100L
 
                 handler.broadcastToRoom(id, GameCountdownUpdateResponse(secondsBeforeStart))
 
