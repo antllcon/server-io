2905a5e70d861b2d995b40c8588179136a75820d
antllcon <pokemonivan32@gmail.com>
2025-07-25 06:45:27 +0300

    Фикс

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -11,7 +11,12 @@ data class Player(
     @kotlinx.serialization.Transient
     val session: WebSocketSession,
     var roomId: String? = null,
-    val car: Car = Car(id = id, playerName = name, initialPosition = Vector2D(5f, 5f)),
+    val car: Car = Car(
+        position = Vector2D(5.4f, 5.5f),
+        id = "Temp ID",
+        direction = 0f,
+        visualDirection = 0f
+    ),
     var ringsCrossed: Int = 0,
     var secondsAfterStart: Float = 0f
 )
